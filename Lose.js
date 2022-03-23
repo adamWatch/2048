ee5b89d3bb61a2dc597c50719d3b60f45d0e06df
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-23 14:48:23 +0100

    change text

--- Lose.js
+++ Lose.js
@@ -1,10 +1,9 @@
 export default class Win {
 	constructor(body, score) {
 		body.innerHTML = `<div class="lose">
-		<h1>You Win!!!</h1>
-		<h2>Congratulations</h2>
+		<h1>You Lose!!!</h1>
 		<h3>Score: ${score}</h3>
-		<h3>Pass: </h3>
+        <h2>Try Again</h2>
 		<button id="reset">Reset</button>
 	</div>`;
 
