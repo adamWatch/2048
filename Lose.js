b8c5c1c5fbdd8bb9de681e75c53451b5b395c013
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-23 14:35:41 +0100

    add lose class

---
+++ Lose.js
@@ -0,0 +1,16 @@
+export default class Win {
+	constructor(body, score) {
+		body.innerHTML = `<div class="lose">
+		<h1>You Win!!!</h1>
+		<h2>Congratulations</h2>
+		<h3>Score: ${score}</h3>
+		<h3>Pass: </h3>
+		<button id="reset">Reset</button>
+	</div>`;
+
+		const reset = document.getElementById("reset");
+		reset.addEventListener("click", () => {
+			location.reload();
+		});
+	}
+}
