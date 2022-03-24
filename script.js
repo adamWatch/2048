809ee9d68a7294b87a64635419b9cd379af233c6
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-24 15:06:26 +0100

    add touch Control

--- script.js
+++ script.js
@@ -14,8 +14,33 @@ grid.randomEmptyCell().tile = new Tile(gameBoard);
 const score = new Score();
 setupInput();
 
+//Touch Listener
+const coordinates = [];
+//touch
+
 function setupInput() {
 	window.addEventListener("keydown", handleInput, { once: true });
+	//touch
+	gameBoard.addEventListener(
+		"touchstart",
+		(e) => {
+			coordinates.splice(0, coordinates.length);
+			checkTouchStart(e);
+		},
+		{ once: true }
+	);
+
+	gameBoard.addEventListener(
+		"touchend",
+		(e) => {
+			checkTouchEnd(e);
+			console.log(coordinates);
+			const direction = setDirection();
+			moveInDirection(direction);
+		},
+		{ once: true }
+	);
+	//touch
 }
 
 async function handleInput(e) {
@@ -52,7 +77,43 @@ async function handleInput(e) {
 			setupInput();
 			return;
 	}
-
+	//touch Switch
+	async function moveInDirection(direction) {
+		switch (direction) {
+			case "up":
+				if (!canMoveUp()) {
+					setupInput();
+					return;
+				}
+				await moveUp();
+				break;
+			case "down":
+				if (!canMoveDown()) {
+					setupInput();
+					return;
+				}
+				await moveDown();
+				break;
+			case "left":
+				if (!canMoveLeft()) {
+					setupInput();
+					return;
+				}
+				await moveLeft();
+				break;
+			case "right":
+				if (!canMoveRight()) {
+					setupInput();
+					return;
+				}
+				await moveRight();
+				break;
+			default:
+				setupInput();
+				return;
+		}
+	}
+	//touch
 	grid.cells.forEach((cell) => cell.mergeTiles());
 
 	const newTile = new Tile(gameBoard);
@@ -160,3 +221,51 @@ function isLose(newTile) {
 		return;
 	}
 }
+
+//Touch Control
+function checkTouchStart(e) {
+	e.preventDefault();
+
+	const x = e.touches[0].clientX;
+	const y = e.touches[0].clientY;
+	console.log(`xSTART = ${x} ; ySTART= ${y}`);
+	coordinates.push(x, y);
+}
+
+function checkTouchEnd(e) {
+	const x = e.changedTouches[0].clientX;
+	const y = e.changedTouches[0].clientY;
+	console.log(`xEND = ${x} ; yEND= ${y}`);
+	coordinates.push(x, y);
+}
+
+function setDirection() {
+	const xStart = coordinates[0];
+	const yStart = coordinates[1];
+	const xEnd = coordinates[2];
+	const yEnd = coordinates[3];
+
+	//Move axis X
+	if (Math.abs(xStart - xEnd) > Math.abs(yStart - yEnd)) {
+		console.log("ruch po X");
+		if (xStart > xEnd) {
+			console.log("left");
+			return "left";
+		} else {
+			console.log("right");
+			return "right";
+		}
+	}
+	//Move axis Y
+	else if (Math.abs(xStart - xEnd) < Math.abs(yStart - yEnd)) {
+		console.log("ruch po Y");
+		if (yStart > yEnd) {
+			console.log("up");
+			return "up";
+		} else {
+			console.log("down");
+			return "down";
+		}
+	}
+}
+//touch
